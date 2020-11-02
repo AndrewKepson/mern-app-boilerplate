a962c28089ceda4bb5e6e148f71184d8e96b24de
Boba Fett <60700164+AndrewKepson@users.noreply.github.com>
2020-11-02 06:51:55 -0700

    Added Mongo connection

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ const app = express();
 //Tell Heroku to use either its own port or, if not available, fall back to port 8080
 const PORT = process.env.PORT || 8080;
 
+//Tell mongoose to connect to our database, either our local environment or the production server
+//The first param points to that environment, and the object accepts our options
+mongoose.connect('mongodb://localhost/mern_app',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+//Mongoose listener to tell us if our connection request is successful 
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to database.`)
+});
+
 //Morgan is an HTTP request logger which will log all requests in the terminal as we run our dev server
 app.use(morgan('tiny'));
 
